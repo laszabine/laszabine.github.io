528c9933c1f348cdfb14633991f126ac3b88c1eb
Roman Reitschmied <rombox@reitschmied.at>
2022-01-28 18:17:48 +0100

    disable analytics

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,4 +2,10 @@
 git checkout production
 git pull
 bundle exec jekyll build
+docker run --rm -it \
+  --volume="$PWD:/srv/jekyll" \
+  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+  jekyll/builder:4.2.0 \
+  jekyll build
+
 exit 0
--- _scripts/dev_start.sh
+++ _scripts/dev_start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
-bundle install --path vendor/bundle
-bundle exec jekyll serve --livereload
+docker run --rm -it \
+  --volume="$PWD:/srv/jekyll" \
+  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+  -p 4000:4000 jekyll/jekyll:4.2.0 \
+  jekyll serve
+#bundle install --path vendor/bundle
+#bundle exec jekyll serve --livereload
